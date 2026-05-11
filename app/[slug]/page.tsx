8b8886f324b98c4c09231b4d21aff3ac6c6e350c
Nic <nic@MacBook-Pro-de-Nic.local>
2026-05-10 23:12:24 -0300

    fix short link server redirect initialization

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,16 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
 import { redirect } from 'next/navigation'
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
-
 export default async function Page({
   params,
 }: {
   params: { slug: string }
 }) {
+
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
 
   const { data } = await supabase
     .from('short_links')
